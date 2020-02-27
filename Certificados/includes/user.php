07e47603e92296dce14547c0d1516d2e8f2866e0
naxus1 <naxus1@gmail.com>
2020-02-27 10:54:09 -0500

    It was created function to get the data from user.

--- Certificados/includes/user.php
+++ Certificados/includes/user.php
@@ -6,11 +6,11 @@
 
     class User extends DB {
 
-        private $nombre;
-        private $apellido;
+        private $name;
+        private $lastname;
         private $username;
 
-        //This function validated if a user exist into
+        //This function validated if a user exist inside the DB.
         public function userExists($username)
         {
             $query = $this->connect()->prepare('SELECT * FROM usuarios WHERE username = :user');
@@ -22,6 +22,27 @@
                 return false;
             }
         }
+
+        //This function get the firstname,lastname and username from a user from data base .
+        public function setUser($user)
+        {
+            $query = $this->connect()->prepare('SELECT * FROM usuarios WHERE username = :user');
+            $query->execute(['user' => $user]);
+    
+            foreach ($query as $currentUser) {
+                $this->name = $currentUser['firstname'];
+                $this->lastname = $currentUser['lastname'];
+                $this->username = $currentUser['username'];
+            }
+        }
+
+        //This function return the firstname,lastname and username from a user from data base .
+        public function getNombre()
+        {
+            return ($this->firstname ." " .  $this->lastname);
+        }
+
+
 
     }
 
